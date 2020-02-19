b272444d7c56002724511b365d2873672882acd3
Harald Aarskog <h.aarskog@gmail.com>
2020-02-19 11:23:54 +0100

    Added constraints

--- src/hans_model.py
+++ src/hans_model.py
@@ -5,7 +5,7 @@ import numpy as np
 
 #Set sizes
 J=2
-T=2
+T=5
 N=10
 R=3
 
@@ -14,46 +14,52 @@ B_jn=np.matrix([
         [1,2,3,4,5,6,7,8,9,10]])
 
 Q_ij=np.matrix([
-        [1,1],
-        [1,1]])
+        [0,1],
+        [0,0]])
 
 delta_jt=np.matrix([
-        [1,1],
-        [1,1]])
+        [8,2,2,2,2,2,2,2,2,2],
+        [2,1,2,2,2,2,2,2,2,2]])
 
 try:
 
-    # Create a new model
-    m = gp.Model("mip1")
+    # Model setup
+    m = gp.Model("mip_model")
     #m.setParam('TimeLimit', 60)
-    
-    # Create variables
-    z = m.addVar(vtype=GRB.BINARY, name="z")
+
+    # Variables
     w = m.addVars(J,T,N,name="w")
     c = m.addVars(J,T,N, vtype=GRB.INTEGER, name="c")
 
-    # Set objective
-    m.setObjective(sum(w[j,t,n]*B_jn[j,n] for j in range(J) for t in range (T) for n in range(N) ), GRB.MINIMIZE)
+    # Objective function
+    m.setObjective(gp.quicksum(w[j,t,n]*B_jn[j,n] for j in range(J) for t in range (T) for n in range(N) ), GRB.MINIMIZE)
 
- 
-    #for j in range(0,J):
-        #for n in range(N):
-            #m.addConstr(sum(w[j,t,n] for t in range(0,T))>= 50, "constraint 1")
-        
+
+    # Constraints
+
+    #CONSTRAINT 2 IN HANS
     for j in range(J):
-        for t in range(T):          
-            m.addConstr(w[j,t,0]==delta_jt[j,t]+sum(c[i,t,n]*Q_ij[i,j] for i in range(J) for n in range(N)))
+        for t in range(T):
+            m.addConstr(w[j,t,0]==delta_jt[j,t]+gp.quicksum(c[i,t,n]*Q_ij[i,j] for i in range(J) for n in range(N)))
 
+    #CONSTRAINT 3 IN HANS
     for j in range(J):
         for t in range(1,T):
             for n in range(1,N):
                 m.addConstr(w[j,t,n] == w[j,t-1,n-1] - c[j,t-1,n-1])
-                
+
+    #CONSTRAINT 4 IN HANS
     for j in range(J):
         for t in range(T):
             for n in range(N):
                 m.addConstr(c[j,t,n] <= w[j,t,n])
+    #CONSTRAINT egendefinert
+    for j in range(J):
+        for t in range(T):
+            for n in range(N):
+                m.addConstr(c[j,t,n] <= 2)
 
+    #Start values
     #w[0,0,0].start=10
     #w[1,0,0].start=10
     # Optimize model
@@ -62,7 +68,6 @@ try:
     for v in m.getVars():
         if v.x>0:
             print('%s %g' % (v.varName, v.x))
-
     print('Obj: %g' % m.objVal)
 
 except gp.GurobiError as e:
@@ -70,3 +75,5 @@ except gp.GurobiError as e:
 
 except AttributeError:
     print('Encountered an attribute error')
+
+m.write("model.lp")
